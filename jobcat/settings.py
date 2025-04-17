cf054dc94af1591449beb23269cfa9275a5e0fb4
naidu62 <vn04081994@gmail.com>
2025-04-18 03:48:05 +0530

    Update settings.py for Render production deployment

--- jobcat/settings.py
+++ jobcat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_kz1cnankd(djb)q784@9@m7x0j9)d0!z-e--hmjkm!bj&++m+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_kz1cnankd(djb)q784@9@m7x0j9)d0!z-e--hmjkm!bj&++m+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev.jobcat.in', '127.0.0.1']
 
 
 # Application definition
@@ -36,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 
-    'myapp',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
